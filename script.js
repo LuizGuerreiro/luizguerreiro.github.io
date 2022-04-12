2680ef4c0d237bf99f093849e94bc3adaf9f353a
luizfrguerreiro@gmail.com <luizfrguerreiro@gmail.com>
2022-04-11 21:12:10 -0300

    consumindo dados do github

--- script.js
+++ script.js
@@ -17,4 +17,51 @@ function getImage() {
   }).catch(e => console.log(e))
 }
 
+function getRepos() {
+  fetch('https://api.github.com/users/luizguerreiro/repos')
+  .then(async res => {
+    if(!res.ok) {
+      throw new Error(res.status)
+    }
+    
+    let dataApi = await res.json()
+    
+    let h3 = document.querySelectorAll('h3')
+    let i = dataApi.length
+    h3.forEach(item => {
+      i--
+      item.innerHTML = dataApi[i].name
+    })
+    
+    let desc = document.querySelectorAll('.description')
+    i = dataApi.length
+    desc.forEach(item => {
+      i--
+      item.innerHTML = dataApi[i].description
+    })
+    
+    let stars = document.querySelectorAll('.projectStars')
+    i = dataApi.length
+    stars.forEach(item => {
+      i--
+      item.innerHTML = dataApi[i].stargazers_count
+    })
+    
+    let forks = document.querySelectorAll('.projectForks')
+    i = dataApi.length
+    forks.forEach(item => {
+      i--
+      item.innerHTML = dataApi[i].forks_count
+    })
+    
+    let techs = document.querySelectorAll('.tech')
+    i = dataApi.length
+    techs.forEach(item => {
+      i--
+      item.innerHTML = dataApi[i].language
+    })
+  })
+}
+
 getImage()
+getRepos()
